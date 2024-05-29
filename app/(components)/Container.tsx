1d074870a109676525e29688da58f9d4c305ec68
Quentin R <55890910+Akariiinnn@users.noreply.github.com>
2024-05-29 14:37:37 +0200

    Revert "Spending list cards"

--- app/(components)/Container.tsx
+++ app/(components)/Container.tsx
@@ -1,19 +1,15 @@
-import {Box, HStack, VStack} from "@gluestack-ui/themed";
+import {Box, HStack} from "@gluestack-ui/themed";
 import {Pie} from "@/app/(components)/Pie";
 import React from "react";
-import SpendingList from "@/app/(components)/SpendingList";
-import Keys from "@/app/(components)/Keys";
+import {SpendingsList} from "@/app/(components)/SpendingList";
 
 export function Container() {
     return (
-        <Box flex={1} bg="var(--background-start-rgb)">
+        <Box flex={1} bg="var(--background-start-rgb)" $web-h={'100vh'}>
             <h1 color={"var(--foreground-rgb)"}>Arrête d'être pauvre </h1>
             <HStack>
-                <VStack>
-                    <Pie />
-                    <Keys />
-                </VStack>
-                <SpendingList/>
+                <Pie/>
+                <SpendingsList/>
             </HStack>
         </Box>
     );
