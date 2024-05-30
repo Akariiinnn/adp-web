26da0ad859f059e520704ed85c6fb225170d4c1f
Laura <Laura@MacBook-Pro-de-Thibault.local>
2024-05-30 10:23:50 +0200

    Add spendings inputs

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import {Container} from "@/app/(components)/Container";
-import UserSettings from "@/app/(components)/UserSettings";
+import AddSpendings from "@/app/(components)/AddSpendings";
 
 export default function Home() {
     return (
